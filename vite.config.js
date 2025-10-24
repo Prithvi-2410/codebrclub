2be457a81e65b14b3ae431501170fb0785763879
Prithvi-2410 <prithviahuja2006@gmail.com>
2025-10-24 20:38:15 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true, // listen on all network interfaces
+    port: 5173,
+    allowedHosts: ["code-br-latest.onrender.com"] // allow Render host
+  }
+})
